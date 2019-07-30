cfa1675a282b5b5d743f27befe78c470d6aa21af
Luke Liem <luke8liem@gmail.com>
2019-07-30 15:59:05 -0700

    Add more documentation to multiprocessing_env.py

--- ppo/lib/multiprocessing_env.py
+++ ppo/lib/multiprocessing_env.py
@@ -17,35 +17,56 @@ from multiprocessing import Process, Pipe
 #   Pipe() - returns a pair of connection objects connected by a duplex (two-way) pipe.
 #     Each connection object has send() and recv() methods (among others).
 
+# OpenAI implements vectorized environs by pairing a parent to a worker using Pipe().
+
 
 def worker(remote, parent_remote, env_fn_wrapper):
-    
-    parent_remote.close()
-    env = env_fn_wrapper.x()
+    # In this function, the worker receives a cmd from its parent on the other end of Pipe and
+    # acts upon the cmd.
+
+    parent_remote.close()  # close the connection on the parent's end
+    env = env_fn_wrapper.x() 
 
     while True:
 
-        cmd, data = remote.recv()  # get cmd and data
+        cmd, data = remote.recv()  # worker receives cmd and data from parent
 
         if cmd == 'step':
+            # worker steps thru environ
             ob, reward, done, info = env.step(data)
+            
             if done:
-                ob = env.reset()
-            remote.send((ob, reward, done, info))
+                ob = env.reset()  # reset environ if done
+
+            # worker sends ob, reward, done, info to parent
+            remote.send((ob, reward, done, info))  
+
         elif cmd == 'reset':
-            ob = env.reset()
-            remote.send(ob)
+            # worker resets environ
+            ob = env.reset() 
+
+            # worker sends ob back to parent
+            remote.send(ob) 
+
         elif cmd == 'reset_task':
+            # reset environ task
             ob = env.reset_task()
+
+            # worker sends ob back to parent
             remote.send(ob)
+
         elif cmd == 'close':
+            # worker closes connection, then break to exit
             remote.close()
             break
+
         elif cmd == 'get_spaces':
-            # send the environ's observation and actions
+            # worker sends the environ's obs_space and action_space to parent
             remote.send((env.observation_space, env.action_space))
+
         else:
             raise NotImplementedError
+
 
 class VecEnv(object):
     """
@@ -105,6 +126,7 @@ class CloudpickleWrapper(object):
     """
     def __init__(self, x):
         self.x = x
+
     def __getstate__(self):
         import cloudpickle
         return cloudpickle.dumps(self.x)
@@ -133,17 +155,19 @@ class SubprocVecEnv(VecEnv):
         self.waiting = False
         self.closed = False
         
+        # For the multiple environs, set up pipes connecting parents (remotes) and workers (work_remotes)
         nenvs = len(env_fns)
         self.nenvs = nenvs
-        
-        # in these environs, remotes and workremotes are connected by two-way pipes
         self.remotes, self.work_remotes = zip(*[Pipe() for _ in range(nenvs)])
 
-        # Set up processes based on the environs, remotes and workremotes. Process() will call
-        # worker function with parameters:
-        #   remote = work_remote
-        #   parent_remote = remote
+        # Set up a list of processes, whereby Process() call worker function with parameters:
+        #   remote = work_remotes (worker)
+        #   parent_remote = remotes (parent)
         #   env_fn_wrapper = CloudpickleWrapper(env_fn)
+
+        # Naming is horrible!!! Going forward, we will use parent and worker to denote the 2 ends of the pipe:
+        #   parent - self.remotes
+        #   worker - self.work_remotes
         self.ps = [Process(target=worker, args=(work_remote, remote, CloudpickleWrapper(env_fn)))
             for (work_remote, remote, env_fn) in zip(self.work_remotes, self.remotes, env_fns)]
         
@@ -153,56 +177,77 @@ class SubprocVecEnv(VecEnv):
             p.daemon = True # Set process to daemon, so if the main process crashes, it will not cause things to hang
             p.start()
         
-        # close the connection on the work_remote end
+        # workers close their connections
         for remote in self.work_remotes:
             remote.close()
 
-        # Get remote work 0 to send back obs and action
+        # parent send 'get_spaces' to worker, and recv from it obs_space and action_space
         self.remotes[0].send(('get_spaces', None))
         observation_space, action_space = self.remotes[0].recv()
 
         # Initialize VecEnv object ???
         VecEnv.__init__(self, len(env_fns), observation_space, action_space)
 
+    """
+    In the methods below, the parents send cmds to their corresponding workers on the other end of the pipes.
+
+    For each cmd, there is a corresponding action in worker().
+    """
+
     def step_async(self, actions):
         # Tell all the environments to start taking a step with the given actions. 
         # Call step_wait() to get the results of the step.
 
-        # Send 'step' cmd to remote workers
+        # Parents send 'step' cmd to workers
         for remote, action in zip(self.remotes, actions):
-            remote.send(('step', action))   # Note that this is blocking pt-2-pt communication
+            remote.send(('step', action))
 
         self.waiting = True   # Set waiting flag to True
 
     def step_wait(self):
         # Wait for the step taken with step_async().
 
-        # Note that blocking pt-2-pt communication is used
+        # Parents wait for results from workers
         results = [remote.recv() for remote in self.remotes]
 
-        self.waiting = False
+        self.waiting = False   # Set waiting flag to False
+
+        # results consist of obs, rewards, dones and infos from workers' environs
         obs, rews, dones, infos = zip(*results)
         return np.stack(obs), np.stack(rews), np.stack(dones), infos
 
     def reset(self):
+        # Parents send 'reset' cmds to works
         for remote in self.remotes:
             remote.send(('reset', None))
+        # Parents wait for obs to come back from workers
         return np.stack([remote.recv() for remote in self.remotes])
 
     def reset_task(self):
+        # Parents send 'reset_task' cmds to works
         for remote in self.remotes:
             remote.send(('reset_task', None))
+        # Parents wait for obs to come back from workers
         return np.stack([remote.recv() for remote in self.remotes])
 
     def close(self):
+
+        # If closed flag is True, return
         if self.closed:
-            return
-        if self.waiting:
+            return    
+
+        # Otherwise
+        if self.waiting:   # if waiting flag is still True
+            # Parents wait to recv results from workers
             for remote in self.remotes:            
                 remote.recv()
+
+        # Parents send 'close' cmds to workers
         for remote in self.remotes:
-            remote.send(('close', None))
-        for p in self.ps:
+            remote.send(('close', None))  
+
+        # Terminate all processes, then set closed flag to True
+        for p in self.ps:   
             p.join()
             self.closed = True
             
